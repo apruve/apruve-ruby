7bbd951d84dd5f86b29e14686e7256b464de51e2
thomascrumrine <tommy@apruve.com>
2017-06-05 11:38:35 -0500

    AP-5495: Added specs for CorporateAccount. Fixed the find method so that it throws NotFound rather than rescuing it.

--- lib/apruve/resources/corporate_account.rb
+++ lib/apruve/resources/corporate_account.rb
@@ -4,12 +4,8 @@ module Apruve
                   :disabled_at, :name, :creditor_term_id, :payment_method_id, :status, :trusted_merchant
 
     def self.find(merchant_id, email)
-      begin
-        response = Apruve.get("merchants/#{merchant_id}/corporate_accounts?email=#{email}")
-        return CorporateAccount.new(response.body[0])
-      rescue Apruve::NotFound
-        return nil
-      end
+      response = Apruve.get("merchants/#{merchant_id}/corporate_accounts?email=#{email}")
+      return CorporateAccount.new(response.body.empty? ? {} : response.body[0])
     end
   end
 end
---
+++ spec/apruve/resources/corporate_account_spec.rb
@@ -0,0 +1,68 @@
+require 'spec_helper'
+
+describe Apruve::CorporateAccount do
+  before :each do
+    Apruve.configure('7ec4e1ae7c96fceba0d599da541912b7', 'local')
+  end
+
+  let (:id) { '89ea2488fe0a5c7bb38aa7f9b088874a' }
+  let (:merchant_uuid) { '89ea2488fe0a5c7bb38aa7f9b088874b' }
+  let (:customer_uuid) { '89ea2488fe0a5c7bb38aa7f9b088874c' }
+  let (:type) { 'CorporateAccount' }
+  let (:payment_term_strategy_name) { 'EOMNet15' }
+  let (:name) { 'A name' }
+  let (:email) { Faker::Internet.email }
+
+  let (:corporate_account) do
+    Apruve::CorporateAccount.new(
+        id: id,
+        merchant_uuid: merchant_uuid,
+        customer_uuid: customer_uuid,
+        type: type,
+        payment_term_strategy_name: payment_term_strategy_name,
+        name: name,
+    )
+  end
+  subject { corporate_account }
+
+  it { should respond_to(:id) }
+  it { should respond_to(:merchant_uuid) }
+  it { should respond_to(:customer_uuid) }
+  it { should respond_to(:type) }
+  it { should respond_to(:created_at) }
+  it { should respond_to(:updated_at) }
+  it { should respond_to(:payment_term_strategy_name) }
+  it { should respond_to(:disabled_at) }
+  it { should respond_to(:name) }
+  it { should respond_to(:creditor_term_id) }
+  it { should respond_to(:payment_method_id) }
+  it { should respond_to(:status) }
+  it { should respond_to(:trusted_merchant) }
+
+
+  describe '#find' do
+    describe 'success' do
+      let! (:stubs) do
+        faraday_stubs do |stub|
+          stub.get("/api/v4/merchants/#{merchant_uuid}/corporate_accounts?email=#{email}") { [200, {}, '{}'] }
+        end
+      end
+      it 'should do a get' do
+        Apruve::CorporateAccount.find(merchant_uuid, email)
+        stubs.verify_stubbed_calls
+      end
+    end
+
+    describe 'not found' do
+      let! (:stubs) do
+        faraday_stubs do |stub|
+          stub.get("/api/v4/merchants/#{merchant_uuid}/corporate_accounts?email=#{email}") { [404, {}, 'Not Found'] }
+        end
+      end
+      it 'should raise' do
+        expect { Apruve::CorporateAccount.find(merchant_uuid, email) }.to raise_error(Apruve::NotFound)
+        stubs.verify_stubbed_calls
+      end
+    end
+  end
+end
