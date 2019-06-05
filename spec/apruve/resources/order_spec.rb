feb254448d1a52aa5e0bd51869108d253cb8a29b
thomascrumrine <tommy@apruve.com>
2019-06-05 12:22:33 -0500

    updateing spec desc

--- spec/apruve/resources/order_spec.rb
+++ spec/apruve/resources/order_spec.rb
@@ -304,7 +304,7 @@ describe Apruve::Order do
     describe '#payment_terms' do
       let (:order) { Apruve::Order.new id: id, merchant_id: 9999, payment_term: payment_term }
 
-      it 'returns payment_terms' do
+      it 'returns payment_term' do
         expect(order.payment_terms).to be order.payment_term
       end
     end
